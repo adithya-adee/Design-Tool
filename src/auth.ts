2506de6108f580b6486610813c81f86db52e4ca8
adithya-adee <adithya25905@gmail.com>
2024-12-08 20:36:52 +0530

    Added Google Authentication For SvleteKit using NextAuthjs

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,14 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
-
-export const { handle } = SvelteKitAuth({
-	providers: []
+import Google from "@auth/sveltekit/providers/google";
+import { env } from "$env/dynamic/private";
+import { SvelteKitAuth } from "@auth/sveltekit";
+export const { handle, signIn, signOut } = SvelteKitAuth({
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      
+    }),
+  ],
+  secret: env.AUTH_SECRET,
+  trustHost: true,
 });
